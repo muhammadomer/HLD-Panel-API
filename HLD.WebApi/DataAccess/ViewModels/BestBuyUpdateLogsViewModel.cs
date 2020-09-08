b1263c86756e31d9bc6ee7a5b51b8c56de58fa74
Abuzar Mehdi <linkabuzarmehdi@gmail.com>
2020-09-08 16:20:05 +0500

    Models

---
+++ HLD.WebApi/DataAccess/ViewModels/BestBuyUpdateLogsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ViewModels
+{
+    public class BestBuyUpdateLogsViewModel
+    {
+        public string SKU { get; set; }
+        public int ProductId { get; set; }
+        public decimal UpdateSelllingPrice { get; set; }
+        public decimal MSRP { get; set; }
+        public string importId { get; set; }
+        public int JobId { get; set; }
+        public string Compress_image { get; set; }
+        public string image_name { get; set; }
+    }
+}
---
+++ HLD.WebApi/DataAccess/ViewModels/BestBuyUpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ViewModels
+{
+   public class BestBuyUpdateViewModel
+    {
+        public int JobID { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? CompletionTime { get; set; }
+        public int TotalProduct { get; set; }
+    }
+}
