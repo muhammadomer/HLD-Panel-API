61a1051ac684d87f3ca8fa5fe731ef5433634533
Mehdi <abuzar.mehdi@phenologix.com>
2021-01-06 18:59:37 +0500

    view model

---
+++ HLD.WebApi/DataAccess/ViewModels/getLogsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ViewModels
+{
+    public class getLogsViewModel
+    {
+        public string trackingNumber { get; set; }
+        public string BBStatus { get; set; }
+        public DateTime shipDate { get; set; }
+        public string scOrderID { get; set; }
+        public string shippingServiceCode { get; set; }
+        public string bbOrderID { get; set; }
+    }
+}
