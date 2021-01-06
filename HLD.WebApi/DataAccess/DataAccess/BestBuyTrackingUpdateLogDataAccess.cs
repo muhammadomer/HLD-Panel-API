5612992d4759fff591d058187900a2c743f626e4
Mehdi <abuzar.mehdi@phenologix.com>
2021-01-06 18:13:42 +0500

    bb tracking logs work done from API side

--- HLD.WebApi/DataAccess/DataAccess/BestBuyTrackingUpdateLogDataAccess.cs
+++ HLD.WebApi/DataAccess/DataAccess/BestBuyTrackingUpdateLogDataAccess.cs
@@ -15,7 +15,8 @@ namespace DataAccess.DataAccess
         public string connStr { get; set; }
         public BestBuyTrackingUpdateLogDataAccess(IConnectionString connectionString)
         {
-            connStr = connectionString.GetPhpConnectionString();
+           // connStr = connectionString.GetPhpConnectionString();
+            connStr = connectionString.GetConnectionString();
         }
         public List<BestBuyTrackingUpdate> GetAllBestBuyUpdateLog()
         {
@@ -83,6 +84,97 @@ namespace DataAccess.DataAccess
             }
             catch (Exception ex)
             {
+            }
+            return listModel;
+        }
+        public int GetLogsCount(string scOrderID, string bbOrderID, string TrakingNumber, string BBStatus, DateTime CurrentDate, DateTime PreviousDate)
+        {
+            int counter = 0;
+            try
+            {
+                if (string.IsNullOrEmpty(scOrderID) || scOrderID == "undefined")
+                    scOrderID = "";
+                if (string.IsNullOrEmpty(bbOrderID) || bbOrderID == "undefined")
+                    bbOrderID = "";
+                if (string.IsNullOrEmpty(TrakingNumber) || TrakingNumber == "undefined")
+                    TrakingNumber = "";
+                if (string.IsNullOrEmpty(BBStatus) || BBStatus == "undefined")
+                    BBStatus = "";
+
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("P_LogsCount", conn);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("_fromDate", PreviousDate );
+                    cmd.Parameters.AddWithValue("_toDate", CurrentDate);
+                    cmd.Parameters.AddWithValue("_scOrderId", scOrderID);
+                    cmd.Parameters.AddWithValue("_bbOrderId", bbOrderID);
+                    cmd.Parameters.AddWithValue("_TrakingNumber", TrakingNumber);
+                    counter = Convert.ToInt32(cmd.ExecuteScalar());
+                    //MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    //DataTable dt = new DataTable();
+                    //da.Fill(dt);
+                    conn.Close();
+                    //counter = dt.Rows.Count;
+                }
+            }
+            catch (Exception exp)
+            {
+
+            }
+            return counter;
+        }
+
+        public List<BestBuyTrackingUpdate> getLogs(string DateTo, string DateFrom, int limit, int offset, string scOrderID, string bbOrderID, string TrakingNumber, string BBStatus)
+        {
+            List<BestBuyTrackingUpdate> listModel = new List<BestBuyTrackingUpdate>();
+            try
+            {
+                if (string.IsNullOrEmpty(scOrderID) || scOrderID == "undefined")
+                    scOrderID = "";
+                if (string.IsNullOrEmpty(bbOrderID) || bbOrderID == "undefined")
+                    bbOrderID = "";
+                if (string.IsNullOrEmpty(TrakingNumber) || TrakingNumber == "undefined")
+                    TrakingNumber = "";
+                if (string.IsNullOrEmpty(BBStatus) || BBStatus == "undefined")
+                    BBStatus = "";
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("P_GetLogs", conn);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("_fromDate", DateFrom);
+                    cmd.Parameters.AddWithValue("_toDate", DateTo);
+                    cmd.Parameters.AddWithValue("_scOrderId", scOrderID);
+                    cmd.Parameters.AddWithValue("_bbOrderId", bbOrderID);
+                    cmd.Parameters.AddWithValue("_TrakingNumber", TrakingNumber);
+                    // cmd.Parameters.AddWithValue("_carrier", BBStatus);
+                    cmd.Parameters.AddWithValue("_limit", limit);
+                    cmd.Parameters.AddWithValue("_offset", offset);
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    da.Fill(dataTable);
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        listModel = new List<BestBuyTrackingUpdate>();
+                        foreach (DataRow dataRow in dataTable.Rows)
+                        {
+                            BestBuyTrackingUpdate model = new BestBuyTrackingUpdate();
+                            model.trackingNumber = Convert.ToString(dataRow["trackingNumber"] != DBNull.Value ? dataRow["trackingNumber"] : "0");
+                            // model.BBStatus = Convert.ToString(dataRow["BBStatus"] != DBNull.Value ? dataRow["BBStatus"] : "0");
+                            model.shipDate = Convert.ToDateTime(dataRow["shipDate"] != DBNull.Value ? dataRow["shipDate"] : DateTime.MinValue);
+                            model.scOrderID = Convert.ToString(dataRow["scOrderID"] != DBNull.Value ? dataRow["scOrderID"] : "0");
+                            model.shippingServiceCode = Convert.ToString(dataRow["shippingServiceCode"] != DBNull.Value ? dataRow["shippingServiceCode"] : "0");
+                            model.bbOrderID = Convert.ToString(dataRow["bbOrderID"] != DBNull.Value ? dataRow["bbOrderID"] : "0");
+                            listModel.Add(model);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
             return listModel;
         }
